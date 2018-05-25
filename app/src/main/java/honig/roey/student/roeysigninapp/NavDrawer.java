dc1f1d596ada7893be08eb37e24ed77291fc7f0d
Roey Honig <roeyhonig94@gmail.com>
2018-05-25 17:13:26 +0300

    change name and lock oriantation

--- app/src/main/java/honig/roey/student/roeysigninapp/NavDrawer.java
+++ app/src/main/java/honig/roey/student/roeysigninapp/NavDrawer.java
@@ -642,6 +642,7 @@ public class NavDrawer extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.nav_drawer, menu);
+        
         return true;
     }
 
